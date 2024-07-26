5835023ce337437a2ef15a8738ca8a60ebc05d46
RamachandranGopalakrishnan <124515368+RamachandranGopalakrishnan@users.noreply.github.com>
2024-07-26 14:23:28 +0530

    Update Averge.java

--- Java Assignment-Basic/Averge.java
+++ Java Assignment-Basic/Averge.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Averge {
     public static void main(String[] args){
         int count=1;
-        int totalWeight=1;
+        int totalWeight=0;
         Scanner sc =new Scanner(System.in);
-        while(count<=10){
+        while(count++<=10){
             System.out.println("Enter the weight of "+count+"persons");
             totalWeight+=sc.nextInt();
         }
